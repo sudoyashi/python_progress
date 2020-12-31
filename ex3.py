9a0a81409912f995ad9896c7e730d82a173e45da
sudoyashi <joshuapdhawaii@gmail.com>
2020-12-30 22:03:25 -1000

    add ex3.py

---
+++ ex3.py
@@ -0,0 +1,30 @@
+print("I will now count my chickens:")  # Print the sentence
+
+print("Hens", 25 + 30 / 6) # Print "Hens", then compute 30/6 + 25 by PEMDAS
+print("Roosters", 100 - 25 * 3 % 4) # Print "Roosters", then compute the expression
+
+print("Now I will count the eggs:") # print
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # PEMDAS expression computation
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+print("Let's try and print floating points")
+floating = float("15")
+print(floating)
+print("Here, we've floated the number 15.")
